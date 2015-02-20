ed08d5198b3016c1d291955d978a674985ffd165
Moncef Belyamani <giter@done>
2015-02-19 23:24:23 -0500

    Test basic conversation tracking with sessions.
    It works!
    
    https://www.twilio.com/docs/quickstart/ruby/sms/tracking-conversations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,28 +1,38 @@
 class LocationsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  def notify
-    twilio_service = TwilioService.new
-    message = twilio_service.send_sms(
-      to: '+12225555555',
-      body: 'Learning to send SMS you are.'
-    )
-
-    render plain: message.status
-  end
-
   def reply
-    twiml = Twilio::TwiML::Response.new do |r|
-      r.Message message_for(params[:Body])
-    end
-    render xml: twiml.text
-  end
+    session[:counter] ||= 0
 
-  def message_for(zip)
-    if zip =~ /\A\d{5}\z/
-      "You entered #{zip}"
-    else
-      "Please enter a valid 5-digit ZIP code"
+    if params[:Body] =~ /reset/i
+      session[:counter] = 0
+      session[:start] = 'true'
     end
+
+    if session[:counter] == 0 || session[:start] == 'true'
+      if params[:Body] =~ /\A\d{5}\z/
+        message = "Here are 5 locations in your zip. Enter a number to choose."
+        session[:zip] = 'true'
+        session[:start] = 'false'
+      else
+        message = "Please enter a valid 5-digit ZIP code"
+      end
+    elsif session[:zip] == 'true'
+      if params[:Body] =~ /\A[1-5]\z/
+        message = "You chose #{params[:Body]}"
+        session[:zip] = 'false'
+      else
+        message = "Please enter a number"
+      end
+    else
+      message = "Please enter a valid 5-digit ZIP code"
+    end
+
+    twiml = Twilio::TwiML::Response.new do |r|
+      r.Message message
+    end
+
+    session[:counter] += 1
+    render xml: twiml.text
   end
 end
